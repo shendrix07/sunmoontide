0a1f2000c417cb6642469a14dcce755b9be4e657
SARA HENDRIX <cruzviz@users.noreply.github.com>
2015-05-01 16:23:21 -0700

    adding newly created tides.py file, taking stuff out of ipython

--- gettimezone.py
+++ gettimezone.py
@@ -5,12 +5,18 @@ import pytz
 from geopy.geocoders import GoogleV3
 geolocator = GoogleV3()
 
-myplace = 'Santa Cruz, Monterey Bay, CA'  ## TODO - get this from file header StationName, State
-
 def getTimeZone(placename):
     mygeocode = geolocator.geocode(myplace)
-mygeocode
-Location(Monterey Bay, United States, (36.8007413, -121.947311, 0.0))
+# mygeocode
+# Location(Monterey Bay, United States, (36.8007413, -121.947311, 0.0))
     mypoint = geopy.Point(mygeocode.latitude, mygeocode.longitude)
     TZ_py = geolocator.timezone(mypoint)
     return TZ_py.zone
+
+
+#myplace = 'Santa Cruz, Monterey Bay, CA'  ## TODO - get this from file header StationName, State
+
+myplace = input('Enter a placename, and I will tell you the Olson time zone.\n')
+
+print(getTimeZone(myplace))
+
