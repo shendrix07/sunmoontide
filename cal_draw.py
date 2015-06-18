c3987f13b09cae03010d7a408fa27656123b2542
SARA HENDRIX <cruzviz@users.noreply.github.com>
2015-06-18 12:13:07 -0700

    more plotting development

--- cal_draw.py
+++ cal_draw.py
@@ -10,7 +10,24 @@ import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 plt.ioff()
 from matplotlib.backends.backend_pdf import PdfPages
+
+import calendar
 import numpy as np
+import pandas as pd
+
+
+def days_in_month(year_month_string):
+    '''Generator that takes year_month_string (i.e. '2015-07') and yields
+    all the days of the month in order, also as strings (i.e. '2015-07-18').
+    '''
+    start_date = pd.to_datetime(year_month_string)
+    _, days_in_month = calendar.monthrange(start_date.year, start_date.month)
+    end_date = start_date + pd.DateOffset(days_in_month)
+    current_date = start_date
+    while current_date < end_date:
+        yield current_date.to_pydatetime().strftime('%Y-%m-%d')
+        current_date = current_date + pd.DateOffset()
+
 
 def generate_annual_calendar(tide_obj, sun_obj, moon_obj, file_name):
     '''Take tide, sun, and moon objects and generate a PDF file named
@@ -27,13 +44,14 @@ def generate_annual_calendar(tide_obj, sun_obj, moon_obj, file_name):
     m = moon_obj.heights
     t_min, t_max = tide_obj.annual_min, tide_obj.annual_max
     place = tide_obj.station_name + ", " + tide_obj.state
+    tz = tide_obj.timezone
 
     pdf_out = PdfPages(file_name)
 
     for i in range(1,13): # for each month number
         this_month = tide_obj.year + "-" + "{:0>2}".format(i)
         monthfig = month_page(t[this_month], s[this_month], m[this_month],
-                              t_min, t_max, place)
+                              t_min, t_max, place, tz)
         print("Month " + str(i) + " figure created, now saving...")
         monthfig.savefig(pdf_out, format='pdf')
         print("Saved month " + str(i))
@@ -42,7 +60,7 @@ def generate_annual_calendar(tide_obj, sun_obj, moon_obj, file_name):
     
     
 def month_page(month_of_tide, month_of_sun, month_of_moon, tide_min, tide_max,
-               place_name):
+               place_name, time_zone):
     '''Takes monthly slices of all_tides, sun heights, and moon heights time
     series, as well as the tide's annual_min and annual_max and station_name,
     and returns a matplotlib.pyplot Figure object containing a month of the
@@ -51,42 +69,46 @@ def month_page(month_of_tide, month_of_sun, month_of_moon, tide_min, tide_max,
     # initialize figure
     fig = plt.figure(figsize=(8.5,11))
     
-    # the zero lines for each variable to be plotted
-    Tz = np.zeros(len(month_of_tide.index))
-    Sz = np.zeros(len(month_of_sun.index))
-    Mz = np.zeros(len(month_of_moon.index))
-    
-    year_month_prefix = month_of_tide.index[0].to_pydatetime().strftime('%Y-%m-')
+    year_month = month_of_tide.index[0].to_pydatetime().strftime('%Y-%m')
     month_title = month_of_tide.index[0].to_pydatetime().strftime('%B')
+    year_title = month_of_tide.index[0].to_pydatetime().strftime('%Y')
+
     
-    # ------------------- SNIP -------------------------
-    def _plot_a_week(week_number, start_time, stop_time):
+    def _plot_a_date(grid_index, date):
         '''Internal function. Works on pre-defined gridspec gs and assumes
         variables like tide_min, tide_max, month_of_tide/moon/sun already
         defined in outer scope.
-        Week numbers start at 1 for the first week of the month and can go up
-        to 6.'''
+        Plots the two daily subplots for `date` in gridspec coordinates
+        gs[gridx, gridy] for the sun/moon and gs[gridx, gridy + 1] for tide.
+        `date` must be in a form pandas recognizes for datetime slicing.
+        i.e. '2015-07-18'
+        '''
+        day_of_sun = month_of_sun[date]
+        day_of_moon = month_of_moon[date]
+        day_of_tide = month_of_tide[date]
         
-        if week_number == 1:
-            gs_ind_1, gs_ind_2 = 0, 1
-        elif week_number == 2:
-            gs_ind_1, gs_ind_2 = 2, 3
-        elif week_number == 3:
-            gs_ind_1, gs_ind_2 = 4, 5
-        elif week_number == 4:
-            gs_ind_1, gs_ind_2 = 6, 7
-        elif week_number == 5:
-            gs_ind_1, gs_ind_2 = 8, 9
-        elif week_number == 6:
-            gs_ind_1, gs_ind_2 = 10, 11
+        # convert indices to matplotlib-friendly datetime format
+        Si = day_of_sun.index.to_pydatetime()
+        Mi = day_of_moon.index.to_pydatetime()
+        Ti = day_of_tide.index.to_pydatetime()
+        
+        # zeros for plotting the filled area under each curve
+        Sz = np.zeros(len(Si))
+        Mz = np.zeros(len(Mi))
+        Tz = np.zeros(len(Ti))
+        
+        # x-limits from midnight to 11:59pm local time
+        start_time = pd.to_datetime(date + ' 00:00').tz_localize(time_zone)
+        start_time = matplotlib.dates.date2num(start_time.to_pydatetime())
+        stop_time = pd.to_datetime(date + ' 23:59').tz_localize(time_zone)
+        stop_time = matplotlib.dates.date2num(stop_time.to_pydatetime())
         
         # sun and moon heights on top
-        ax1 = plt.subplot(gs[gs_ind_1,0])
-        ax1.fill_between(month_of_sun.index, month_of_sun, Sz,
-                         color='#FFEB00', alpha=1)
-        ax1.fill_between(month_of_moon.index, month_of_moon, Mz,
-                         color='#D7A8A8', alpha=0.2)
-        ax1.axis([start_time, stop_time, 0, 1])
+        ax1 = plt.subplot(gs[grid_index])
+        ax1.fill_between(Si, day_of_sun, Sz, color='#FFEB00', alpha=1)
+        ax1.fill_between(Mi, day_of_moon, Mz, color='#D7A8A8', alpha=0.2)
+        ax1.set_xlim((start_time, stop_time))
+        ax1.set_ylim((0, 1))
         ax1.set_xticks([])
         ax1.set_yticks([])
         for axis in ['top','left','right']:
@@ -94,10 +116,10 @@ def month_page(month_of_tide, month_of_sun, month_of_moon, tide_min, tide_max,
         ax1.spines['bottom'].set_visible(False)
         
         # tide magnitudes below
-        ax2 = plt.subplot(gs[gs_ind_2,0])
-        ax2.fill_between(month_of_tide.index, month_of_tide, Tz,
-                         color='#52ABB7', alpha=0.8)
-        ax2.axis([start_time, stop_time, tide_min, tide_max])
+        ax2 = plt.subplot(gs[grid_index + 7])
+        ax2.fill_between(Ti, day_of_tide, Tz, color='#52ABB7', alpha=0.8)
+        ax2.set_xlim((start_time, stop_time))
+        ax2.set_ylim((tide_min, tide_max))
         ax2.set_xticks([])
         ax2.set_yticks([])
         for axis in ['bottom','left','right']:
@@ -105,24 +127,20 @@ def month_page(month_of_tide, month_of_sun, month_of_moon, tide_min, tide_max,
         ax2.spines['top'].set_linewidth(0.5)
     
         
-    gs = gridspec.GridSpec(8, 1, hspace=0.0)
+    gs = gridspec.GridSpec(12, 7, wspace = 0.0, hspace = 0.0)
     
-    w1_first = year_month_prefix + '01 00:00'
-    w1_last = year_month_prefix + '07 23:59'
-    w2_first = year_month_prefix + '08 00:00'
-    w2_last = year_month_prefix + '14 23:59'
-    w3_first = year_month_prefix + '15 00:00'
-    w3_last = year_month_prefix + '21 23:59'
-    w4_first = year_month_prefix + '22 00:00'
-    w4_last = year_month_prefix + '28 23:59'
-    
+    # call _plot_a_date in correct grid location
+    gridnum = (pd.to_datetime(year_month + '-01').dayofweek + 1) % 6
+    for day in days_in_month(year_month):
+        _plot_a_date(gridnum, day)
+        # dayofweek = The day of the week with Monday=0, Sunday=6
+        if pd.to_datetime(day).dayofweek == 5: # if just plotted a Saturday
+            gridnum += 8  # skip down a week to leave tide subplots intact
+        else:
+            gridnum += 1
 
-    _plot_a_week(1, w1_first, w1_last)
-    _plot_a_week(2, w2_first, w2_last)
-    _plot_a_week(3, w3_first, w3_last)
-    _plot_a_week(4, w4_first, w4_last)
 
-    # ---------------------------------------------------
-    fig.suptitle(month_title + ' in ' + place_name, fontsize='x-large',
+    # add annotations and titles
+    fig.suptitle(month_title + '   ' + year_title, size='72',
                  fontname='Foglihten')
     return fig
